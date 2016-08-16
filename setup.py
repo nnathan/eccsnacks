33021c33f847550b5a6368f7e90bb510acf29140
Naveen Nathan <nnathan@gmail.com>
2016-08-17 04:21:33 +1000

    tests: remove tox

--- setup.py
+++ setup.py
@@ -1,29 +1,5 @@
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 import sys
-
-
-class Tox(TestCommand):
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        errno = tox.cmdline(args=args)
-        sys.exit(errno)
 
 
 setup(
@@ -40,8 +16,8 @@ setup(
 
     install_requires=[],
 
-    tests_require=['tox'],
-    cmdclass={'test': Tox},
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 
     keywords = ['curve25519', 'curve448', 'goldilocks', 'rfc7748'],
     classifiers=[
